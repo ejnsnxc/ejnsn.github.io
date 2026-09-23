edc61591506f2ace8e2b0f45d80264b1fc554141
Ejnsn <ejnsnxc@gmail.com>
2026-09-23 10:29:08 -0600

    Completed Assignment 4

--- TempConvertStarter/scripts/convert_temp.js
+++ TempConvertStarter/scripts/convert_temp.js
@@ -2,37 +2,75 @@ window.addEventListener("DOMContentLoaded", domLoaded);
 
 // When the DOM has finished loading, add the event listeners.
 function domLoaded() {
-   // TODO: Use addEventListener() to register a click event handler for the convert button.
-   // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#add_a_simple_listener
+   const F_input = document.getElementById("F_in");
+   const C_input = document.getElementById("C_in");
+   const convertBtn = document.getElementById("convertButton");
 
-   // Add event listeners to handle clearing the box that WAS NOT clicked,
-   // e.g., the element C_in listens for 'input', with a callback fn to
-   // execute after that event does happen. 
-   // You don't send arguments to the event handler function.
-   // So, if you want the event handler to call another function that
-   // DOES take arguments, you can send that other function as a callback.
-   // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#event_listener_with_anonymous_function
-   // Here is an example of anonymous event handler fn that calls alert with an argument:
-   // document.getElementById("weatherIcon").addEventListener("click", function() {alert("You clicked the icon.")})
+   F_input.addEventListener("input", clearC);
+   C_input.addEventListener("input", clearF);
 
+   convertBtn.addEventListener("click", convertTemp);
 }
-// TODO: (Part of the above is to write the functions to be executed when the event handlers are invoked.)
+
+
+function clearC(){
+   document.getElementById("C_in").value = "";
+}
+function clearF(){
+   document.getElementById("F_in").value = "";
+}
+
+function convertTemp(){
+   const F_input = document.getElementById("F_in");
+   const C_input = document.getElementById("C_in");
+   const message = document.getElementById("message");
+
+   if(C_input.value.length > 0){
+      message.textContent = "";
+      const Ctemp = parseFloat(C_input.value);
+      const Ftemp = convertCtoF(Ctemp);
+      F_input.value = Ftemp;
+      weatherIcon(Ftemp);
+   }
+   else if (F_input.value.length > 0){
+      message.textContent = "";
+      const Ftemp = parseFloat(F_input.value);
+      const Ctemp = convertFtoC(Ftemp);
+      C_input.value = Ctemp;
+      weatherIcon(Ftemp);
+   }
+   else{
+      message.textContent = "Enter a temperature to convert";
+      weatherIcon(null);
+   }
+}
+
 
 function convertCtoF(C) {
-   // TODO: Return temp in °F. 
-   // °F = °C * 9/5 + 32
+   return C * (9/5) + 32;
 }
 
 function convertFtoC(F) {
-   // TODO: Return temp in °C. 
-   // °C = (°F - 32) * 5/9
+   return (F - 32) * (5/9);
 }
 
-// TODO: write a fn that can be called with every temp conversion
-// to display the correct weather icon.
-// Based on degrees Fahrenheit:
-// 32 or less, but above -200: cold
-// 90 or more, but below 200: hot
-// between hot and cold: cool
-// 200 or more, -200 or less: dead
-// both input fields are blank: C-F
+
+function weatherIcon(Ftemp){
+   const icon = document.getElementById("weatherIcon");
+
+   if(Ftemp === null){
+      icon.src = "images/C-F.png";
+   }
+   else if(Ftemp <= 32 && Ftemp > -200){
+      icon.src = "images/cold.png";
+   }
+   else if(Ftemp >= 90 && Ftemp < 200){
+      icon.src = "images/hot.png";
+   }
+   else if(Ftemp >= 200 || Ftemp <= -200){
+      icon.src = "images/dead.png";
+   }
+   else{
+      icon.src = "images/cool.png";
+   }
+}
